4954bb3a10591e9e80692d579b664e9ac0d7a7ad
Julien Sagot <ju.sagot@gmail.com>
2014-07-18 10:41:26 +0200

    Better error message.

--- src/main.ml
+++ src/main.ml
@@ -1,5 +1,17 @@
+let parsing_err lexbuf =
+  let pos = Lexing.lexeme_start_p lexbuf in
+  let msg =
+    Printf.sprintf "Parsing error: line %d, column %d, character '%s'"
+                   pos.Lexing.pos_lnum
+                   (pos.Lexing.pos_cnum - pos.Lexing.pos_bol)
+                   (Lexing.lexeme lexbuf)
+  in failwith msg
+
 let _ =
-  Lexing.from_channel stdin
-  |> Llvm_parser.module_ Llvm_lexer.token
-  |> Printer.pprint
-  |> print_string
+  let lexbuf = Lexing.from_channel stdin in
+  try
+    lexbuf
+    |> Llvm_parser.module_ Llvm_lexer.token
+    |> Printer.pprint
+    |> print_string
+  with Llvm_parser.Error -> parsing_err lexbuf
